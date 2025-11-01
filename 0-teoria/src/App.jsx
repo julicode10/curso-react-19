8da14a8f82092eb6972ce232a9667c6c59ecdb9b
julicode10 <jlondono433@gmail.com>
2025-11-01 12:29:36 -0500

    map de arrays

--- 0-teoria/src/App.jsx
+++ 0-teoria/src/App.jsx
@@ -1,12 +1,19 @@
-import { HolaMundo } from './components/HolaMundo'
-import { Contador } from './components/Contador'
+import { HolaMundo } from "./components/HolaMundo";
+import { Contador } from "./components/Contador";
 function App() {
+  const frutas = ["manzana", "banana", "cereza", "uva", "naranja"];
   return (
     <div className="bg-black text-white h-screen">
-        <HolaMundo />
-        <Contador />
+      <HolaMundo />
+      <Contador />
+
+      {frutas.map((fruta, index) => (
+        <div key={index} className="bg-amber-300">
+          <h2 className="text-2xl font-bold">{fruta}</h2>
+        </div>
+      ))}
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
