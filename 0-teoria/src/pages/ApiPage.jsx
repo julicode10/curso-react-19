19ccd6e7d9de718e1975207af5f6a97dee6ac2f8
julicode10 <jlondono433@gmail.com>
2025-11-02 14:46:33 -0500

    mapeando listado de pokemones

--- 0-teoria/src/pages/ApiPage.jsx
+++ 0-teoria/src/pages/ApiPage.jsx
@@ -2,30 +2,23 @@ import { useState, useEffect } from "react";
 export const ApiPage = () => {
   const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
       .then((response) => response.json())
       .then((data) => {
-        setPokemons(data);
-        console.log(data);
+        setPokemons(data.results);
+        console.log(data.results);
       });
   }, []);
   return (
     <div className="h-screen bg-amber-400 text-black">
       <h1>ApiPage</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {pokemons?.abilities?.length > 0 && (
-          <div>
-            <h2>{pokemons.abilities[0].ability.name}</h2>
+      <section className="flex flex-col">
+        {pokemons.map((pokemon, index) => (
+          <div key={index}>
+            <h2>{pokemon.name}</h2>
           </div>
-        )}
-        {pokemons?.stats?.length > 0 && (
-          <div>
-            <h2>{pokemons.stats[3].base_stat}</h2>
-          </div>
-        )}
-
-        {pokemons.weight}
-      </div>
+        ))}
+      </section>
     </div>
   );
 };
