89114a5167babed441ccbdc25d97fa1cfcb9fd47
julicode10 <jlondono433@gmail.com>
2025-11-02 14:54:27 -0500

    usando axios

--- 0-teoria/src/pages/ApiPage.jsx
+++ 0-teoria/src/pages/ApiPage.jsx
@@ -1,24 +1,46 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 export const ApiPage = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
-      .then((response) => response.json())
-      .then((data) => {
-        setPokemons(data.results);
-        console.log(data.results);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+      .then((response) => {
+        setPokemons(response.data.results);
+        console.log(response.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
       });
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setPokemons(data.results);
+    //     console.log(data.results);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   }, []);
   return (
     <div className="h-screen bg-amber-400 text-black">
       <h1>ApiPage</h1>
-      <section className="flex flex-col">
-        {pokemons.map((pokemon, index) => (
-          <div key={index}>
-            <h2>{pokemon.name}</h2>
-          </div>
-        ))}
-      </section>
+      {loading ? (
+        <div>
+          <h2>Cargando...</h2>
+        </div>
+      ) : (
+        <section className="flex flex-col">
+          {pokemons.map((pokemon, index) => (
+            <div key={index}>
+              <h2>{pokemon.name}</h2>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 };
